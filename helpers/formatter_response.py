5b61f4541358da8d0b19da9b699efabcfa65ce54
Wellington Umbelino <wellington.s.umbelino@gmail.com>
2023-11-06 21:15:39 -0300

    Ajustando função da resposta ao usuário

--- helpers/formatter_response.py
+++ helpers/formatter_response.py
@@ -2,4 +2,4 @@ def format_response(response):
   if response is None:
     print("Desculpe, eu não entendi a sua pergunta.\n")
   else:
-    print('\nOk, aqui está o seu código.\n\n', str(response))
+    print('\nOk, aqui está o seu código.\n\n', str(response), '\n')
