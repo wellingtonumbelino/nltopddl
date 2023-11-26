19a4c3fce49e91d9b5ee48aedfbb96edd0f9bf21
Wellington Umbelino <wellington.s.umbelino@gmail.com>
2023-11-26 15:21:48 -0300

    Ajustando espaçamento de linha para resposta

--- helpers/formatter_response.py
+++ helpers/formatter_response.py
@@ -2,6 +2,6 @@ def format_response(response):
   if response is None:
     print("Desculpe, eu não entendi a sua pergunta.\n")
   elif "Huuum" in response:
-    print(response, '\n')
+    print('\n', response, '\n')
   else:
     print('\nOk, aqui está o seu código.\n\n', str(response), '\n')
